e055b15ab16934b2505cedfebba6e2629bebf903
prabhasavvari <214g5a3202@srit.ac.in>
2024-11-22 21:42:15 +0530

    Navbar-component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <h1>welcome!</h1>
+    <Navbar />
   );
 }
 
